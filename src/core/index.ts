6827bfb966bd33271087619830756ec5dfe1d793
Enes Ozer <enesozer.f@gmail.com>
2022-08-18 09:37:27 +0300

    Update index.ts

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,4 +3,5 @@ export {Draw} from './libs/draw'
 export {Game} from './libs/game'
 export {Store} from './libs/store'
 export {Vector2D} from './libs/vector2D'
+export {Line} from './objects/line'
 //#endregion
